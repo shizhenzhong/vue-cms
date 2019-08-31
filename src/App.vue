1a0363de6712fe13c07e60bad0a3cad2e0845f7e
shizhenzhong <shizhenzhong2013@qq.com>
2019-08-31 17:45:19 +0800

    实现九宫格布局和tabbar按钮切换动画效果

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,10 @@
       
        <mt-header fixed title="豆瓣电影"></mt-header>
          <!--中部-->
-          <router-view></router-view>
+		 <transition>
+            <router-view></router-view>
+		 </transition>
+        
           <!--底部-->
           <nav class="mui-bar mui-bar-tab">
 			<router-link class="mui-tab-item" to="/home">
@@ -35,5 +38,20 @@ export default {
 <style lang="scss" scoped>
   .app-container{
       padding-top:40px;
+	  overflow-x: hidden;
+  }
+  .v-enter{
+	  opacity: 0;
+	  transform: translateX(100%);
+	  
+  }
+  .v-leave-to{
+	  opacity: 0;
+	  transform: translateX(-100%);
+	  position: absolute;
+
+  }
+  .v-enter-active{
+	  transition: all 0.5s ease; 
   }
 </style>
--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -6,7 +6,29 @@
             </mt-swipe-item>
            
         </mt-swipe>
-        <h1>HomeContainer</h1>
+
+       <ul class="mui-table-view mui-grid-view mui-grid-9">
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <img src="../../images/menu1.png"/>
+		                    <div class="mui-media-body">新闻资讯</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <img src="../../images/menu2.png"/>
+		                    <div class="mui-media-body">图片分享</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                     <img src="../../images/menu3.png"/>
+		                    <div class="mui-media-body">商品购买</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                     <img src="../../images/menu4.png"/>
+		                    <div class="mui-media-body">留言反馈</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                    <img src="../../images/menu5.png"/>
+		                    <div class="mui-media-body">视频专区</div></a></li>
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		                     <img src="../../images/menu6.png"/>
+		                    <div class="mui-media-body">联系我们</div></a></li>
+		            
+		        </ul> 
+        
     </div>
 </template>
 <script>
@@ -37,6 +59,7 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+    
     .mint-swipe{
         height: 200px;
         .mint-swipe-item{
@@ -54,5 +77,18 @@ export default {
                 height: 100%;
             }
         }
+    }
+    .mui-grid-view.mui-grid-9{
+        background-color: #fff;
+        border: 0;
+    }
+    .mui-grid-view.mui-grid-9 .mui-table-view-cell{
+        background-color: #fff;
+        border: 0;
+        img{
+            width: 50%;
+            height: 50%;
+        }
+
     }
 </style>
