ad9b01a84c8f771559ec463f084cbd66155cc3fd
shizhenzhong <shizhenzhong2013@qq.com>
2019-08-31 16:49:54 +0800

    实现图片轮播

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -1,13 +1,58 @@
 <template>
     <div>
+        <mt-swipe :auto="4000">
+            <mt-swipe-item v-for="item in lunbotuList" :key="item.url">
+                <img :src="item.url" alt="" >
+            </mt-swipe-item>
+           
+        </mt-swipe>
         <h1>HomeContainer</h1>
     </div>
 </template>
 <script>
+import { Toast } from 'mint-ui';
+
+
 export default {
-    
+    data(){
+        return {
+            lunbotuList:[]
+        }
+    },
+    created(){
+       this.getLunbotu()
+    },
+    methods:{
+        getLunbotu(){
+        this.$http.get("http://vue.study.io/api/getlunbo").then(result=>{
+            if(result.body.status===0){
+                this.lunbotuList=result.body.message;
+            }else{
+                Toast('获取图片失败');
+            }
+        })
+        }
+    }
+
 }
 </script>
 <style lang="scss" scoped>
-    
+    .mint-swipe{
+        height: 200px;
+        .mint-swipe-item{
+            &:nth-child(1){
+                background-color: red;
+            }
+            &:nth-child(2){
+                background-color: green;
+            }
+            &:nth-child(3){
+                background-color: aquamarine;
+            }
+            img{
+                width:100%;
+                height: 100%;
+            }
+        }
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 
 
 import Vue from 'vue'
-import VueRouter from 'vue-router' //导入vue路由组件
-import { Header } from 'mint-ui'; 
+
+import { Header,Swipe, SwipeItem } from 'mint-ui'; 
 import 'mint-ui/lib/style.min.css'//导入mint-ui组件css
 import './lib/mui/css/mui.min.css' //导入minui组件css
 import './lib/mui/css/icons-extra.css'
@@ -10,10 +10,18 @@ import './lib/mui/css/icons-extra.css'
 
 
 //注册mint-ui组件
-Vue.component(Header.name, Header);
+Vue.component(Header.name, Header)
+Vue.component(Swipe.name,Swipe)
+Vue.component(SwipeItem.name,SwipeItem)
 
 //注册路由
+import VueRouter from 'vue-router' //导入vue路由组件
 Vue.use(VueRouter)
+
+//导入vue-resource
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
 
 
 import App from './App.vue'
