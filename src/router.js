e28a9870c42474d4d108b9db057b9a89395378be
shizhenzhong <shizhenzhong2013@qq.com>
2019-08-31 16:11:30 +0800

    实现tabbar路由组件的切换

--- src/router.js
+++ src/router.js
@@ -1,10 +1,19 @@
 import VueRouter from 'vue-router'
 
 
+//导入tabbar组件
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
+
 
 var  router=new VueRouter({
     routes:[
-        
+        {path:"/home",component:HomeContainer},
+        {path:"/member",component:MemberContainer},
+        {path:'/shopcar',component:ShopcarContainer},
+        {path:'/search',component:SearchContainer}
     ],
     linkActiveClass:'mui-active'
 })
