432715e9a5e75446713dd7c6a6e05f9be9d6cd7d
shizhenzhong <shizhenzhong2013@qq.com>
2019-09-08 10:14:48 +0800

    获取图片分类列表

--- src/components/photo/photolist.vue
+++ src/components/photo/photolist.vue
@@ -3,24 +3,10 @@
       	<div id="slider" class="mui-slider">
 				<div id="sliderSegmentedControl" class="mui-scroll-wrapper mui-slider-indicator mui-segmented-control mui-segmented-control-inverted">
 					<div class="mui-scroll">
-						<a class="mui-control-item mui-active" href="#item1mobile" data-wid="tab-top-subpage-1.html">
-							推荐
+						<a  :class="['mui-control-item',item.id===0?'mui-active':'']" v-for="item in cates" :key="item.id">
+							{{item.title}}
 						</a>
-						<a class="mui-control-item" href="#item2mobile" data-wid="tab-top-subpage-2.html">
-							热点
-						</a>
-						<a class="mui-control-item" href="#item3mobile" data-wid="tab-top-subpage-3.html">
-							北京
-						</a>
-						<a class="mui-control-item" href="#item4mobile" data-wid="tab-top-subpage-4.html">
-							社会
-						</a>
-						<a class="mui-control-item" href="#item5mobile" data-wid="tab-top-subpage-5.html">
-							娱乐
-						</a>
-						<a class="mui-control-item" href="#item6mobile" data-wid="tab-top-subpage-6.html">
-							科技
-						</a>
+					
 					</div>
 				</div>
 
@@ -30,21 +16,32 @@
 <script>
 
 import mui from '../../lib/mui/js/mui.min.js'
-
+import {Toast} from 'mint-ui'
 
 export default {
     data(){
         return {
-
+           cates:[]
         }
-    },
+	},
+	created(){
+        this.getCategories()
+	},
     mounted(){
         mui('.mui-scroll-wrapper').scroll({
 	       deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
        });
     },
     methods: {
-        
+        getCategories(){
+			this.$http.get("api/getCategory").then(result=>{
+				if(result.body.status===0){
+                       this.cates=result.body.message.unshift({title:"全部",id:0})
+				}else{
+                   Toast("获取图片分类失败")
+				}
+			})
+		}
     },
 }
 </script>
